ae70dec6604f9826da4d4825cef351444bf38c3e
self <riteshshawk.rk@gmail.com>
2021-03-28 00:53:35 +0530

    added two tab feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ulTasks = $('#ulTasks')
+
 let btnAdd = $('#btnAdd')
 let btnCancel = $('#btnCancel')
 let btnSort = $('#btnSort')
@@ -20,41 +20,63 @@ function addToCollection(item) {
     if (collections.hasOwnProperty(collection)) {
       var element = collections[collection];
       if(element.obj.is(':checked')) {
-        element.list.push({value:item.val(), status:f})
+        element.list.push({value:item.val(), status:false})
       }
     }
   }
 }
+
+function activeCollection() {
+  for (var collection in collections) {
+    if (collections.hasOwnProperty(collection)) {
+      var element = collections[collection];
+      if (element.obj.is(':checked')) {
+        return collection
+      }
+    }
+  }
+}
+
+$('.btn-check').click(() => {
+  $('.lists').css('display', 'none')
+  $('#'+activeCollection()).css('display','block')
+})
+
+
+
+
 
 function addItem() {
   let listItem = $('<li>', {
     'class': 'list-group-item',
     text: inpNewTask.val()
   })
+  
+  $('#ulTasks'+activeCollection()).append(listItem)
+  addToCollection(listItem)
   listItem.click(() => {
     listItem.toggleClass('done')
   })
-  ulTasks.append(listItem)
-  addToCollection(listItem)
   
   inpNewTask.val('')
   toggleInputButtons()
 }
 
+
 function clearDone() {
-  $('#ulTasks .done').remove()
+  $('#ulTasks'+activeCollection()+' .done').remove()
   toggleInputButtons()
 }
 
 function sortTasks() {
-  $('#ulTasks .done').appendTo(ulTasks)
+  $('#ulTasks'+activeCollection()+' .done').appendTo($('#ulTasks'+activeCollection()))
 }
 
 function toggleInputButtons() {
   btnCancel.prop('disabled', inpNewTask.val() == '')
   btnAdd.prop('disabled', inpNewTask.val() == '')
-  btnSort.prop('disabled', ulTasks.children().length < 1)
-  btnCleanup.prop('disabled', ulTasks.children().length < 1)
+  btnSort.prop('disabled', $('#ulTasks'+activeCollection()).children().length < 1)
+  btnCleanup.prop('disabled', $('#ulTasks'+activeCollection()).children().length < 1)
 }
 
 inpNewTask.keypress((e) => {

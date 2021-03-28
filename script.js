028210c68da52f8023c975d02e2befe7c76f1c37
Ritesh Kumar <riteshshawk.rk@gmail.com>
2021-03-28 10:58:43 +0000

    added local storage and some ui

--- script.js
+++ script.js
@@ -5,80 +5,111 @@ let btnSort = $('#btnSort')
 let btnCleanup = $('#btnCleanup')
 let inpNewTask = $('#inpNewTask')
 let collections = {
-  Home:{
-    obj:$('#btnHome'),
-    list:[]
+  Home: {
+    list: []
   },
-  Office:{
-    obj:$('#btnOffice'),
-    list:[]
+  Office: {
+    list: []
   }
 }
 
-function addToCollection(item) {
-  for (var collection in collections) {
-    if (collections.hasOwnProperty(collection)) {
-      var element = collections[collection];
-      if(element.obj.is(':checked')) {
-        element.list.push({value:item.val(), status:false})
-      }
-    }
-  }
-}
 
 function activeCollection() {
   for (var collection in collections) {
     if (collections.hasOwnProperty(collection)) {
-      var element = collections[collection];
-      if (element.obj.is(':checked')) {
+      if ($('#btn' + collection).is(':checked')) {
         return collection
       }
     }
   }
 }
 
-$('.btn-check').click(() => {
-  $('.lists').css('display', 'none')
-  $('#'+activeCollection()).css('display','block')
-})
+function load() {
+  if (localStorage.getItem('m-todo')) {
+    collections = JSON.parse(localStorage.getItem('m-todo'))
+    for (var collection in collections) {
+      if (collections.hasOwnProperty(collection)) {
+        $('#ulTasks' + collection+' li').remove()
+        collections[collection].list.forEach(task => {
+          let listItem = $('<li>', {
+            'class': 'list-group-item' + (task.status ? " done" : ""),
+            text: task.value
+          })
+          console.log("name" +(task.status ? "done" : ""))
+          $('#ulTasks' + collection).append(listItem)
+          listItem.click((e) => {
+            listItem.toggleClass('done')
 
-
-
-
-
+            collections[activeCollection()].list.forEach(task => {
+              if (task.value == e.target.innerText) {
+                task.status = !task.status
+              }
+              save()
+            });
+          })
+        })
+      }
+    }
+  }
+}
+function save() {
+  localStorage.setItem("m-todo", JSON.stringify(collections))
+}
 function addItem() {
   let listItem = $('<li>', {
     'class': 'list-group-item',
     text: inpNewTask.val()
   })
-  
-  $('#ulTasks'+activeCollection()).append(listItem)
-  addToCollection(listItem)
-  listItem.click(() => {
+
+  $('#ulTasks' + activeCollection()).append(listItem)
+  collections[activeCollection()].list.push({ "value": inpNewTask.val(), status: false })
+  save()
+
+  listItem.click((e) => {
     listItem.toggleClass('done')
+
+    collections[activeCollection()].list.forEach(task => {
+      if (task.value == e.target.innerText) {
+        task.status = !task.status
+      }
+    });
+    save()
   })
-  
+
   inpNewTask.val('')
   toggleInputButtons()
 }
-
-
+function removeDone(arr, val) {
+  let i = arr.indexOf(val)
+  arr.splice(i, 1)
+  return arr
+}
 function clearDone() {
-  $('#ulTasks'+activeCollection()+' .done').remove()
+  $('#ulTasks' + activeCollection() + ' .done').remove()
+  collections[activeCollection()].list.forEach(task => {
+    if(task.status) {
+      collections[activeCollection()].list = removeDone(collections[activeCollection()].list, task)
+    }
+  })
+  save()
   toggleInputButtons()
 }
-
 function sortTasks() {
-  $('#ulTasks'+activeCollection()+' .done').appendTo($('#ulTasks'+activeCollection()))
+  $('#ulTasks' + activeCollection() + ' .done').appendTo($('#ulTasks' + activeCollection()))
 }
-
 function toggleInputButtons() {
   btnCancel.prop('disabled', inpNewTask.val() == '')
   btnAdd.prop('disabled', inpNewTask.val() == '')
-  btnSort.prop('disabled', $('#ulTasks'+activeCollection()).children().length < 1)
-  btnCleanup.prop('disabled', $('#ulTasks'+activeCollection()).children().length < 1)
+  btnSort.prop('disabled', $('#ulTasks' + activeCollection()).children().length < 1)
+  btnCleanup.prop('disabled', $('#ulTasks' + activeCollection()).children().length < 1)
 }
 
+load()
+$('.btn-check').click(() => {
+  $('.lists').css('display', 'none')
+  $('#' + activeCollection()).css('display', 'block')
+})
+$('#btnHome').click()
 inpNewTask.keypress((e) => {
   if (e.which == 13) addItem()
 })
